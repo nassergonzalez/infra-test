fa0b60c362f6717b234f2761dcb4d51645f76abe
nasser.gonzalez@gmail.com <nasser.gonzalez@gmail.com>
2021-12-07 14:08:47 +0000

    test atlantis

--- _infra/main.tf
+++ _infra/main.tf
@@ -6,11 +6,11 @@
 ##  source = "./modules/app2"
 ##}
 #
-resource "aws_s3_bucket" "b" {
+resource "aws_s3_bucket" "b2" {
   bucket = var.bucket_name
   acl    = "public-read"
 }
-resource "aws_instance" "web" {
+resource "aws_instance" "web2" {
   ami           = "ami-0d57c0143330e1fa7"
   instance_type = "m5.8xlarge"
 
