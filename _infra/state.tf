cf7cb6fc70fc509427c22acfdff9d12b3a5b244f
nasser.gonzalez@gmail.com <nasser.gonzalez@gmail.com>
2021-12-13 16:54:35 +0000

    atlantis test

--- _infra/state.tf
+++ _infra/state.tf
@@ -2,18 +2,18 @@ terraform {
 #  backend "local" {
 #    path = "terraform_state/terraform.tfstate"
 #  }
-  backend "http" {
-    address = "http://host.docker.internal:8000/api/v1/project/12345/terraform/state/terraform.state"
-    lock_address = "http://host.docker.internal:8000/api/v1/project/12345/terraform/state/lock/terraform.lock"
-    unlock_address = "http://host.docker.internal:8000/api/v1/project/12345/terraform/state/unlock/terraform.lock"
-    unlock_method = "DELETE"
-    lock_method = "POST"
-  }
 #  backend "http" {
-#    address = "http://localhost:8000/api/v1/project/12345/terraform/state/terraform.state"
-#    lock_address = "http://localhost:8000/api/v1/project/12345/terraform/state/lock/terraform.lock"
-#    unlock_address = "http://localhost:8000/api/v1/project/12345/terraform/state/unlock/terraform.lock"
+#    address = "http://host.docker.internal:8000/api/v1/project/12345/terraform/state/terraform.state"
+#    lock_address = "http://host.docker.internal:8000/api/v1/project/12345/terraform/state/lock/terraform.lock"
+#    unlock_address = "http://host.docker.internal:8000/api/v1/project/12345/terraform/state/unlock/terraform.lock"
 #    unlock_method = "DELETE"
 #    lock_method = "POST"
 #  }
+  backend "http" {
+    address = "http://localhost:8000/api/v1/project/12345/terraform/state/terraform.state"
+    lock_address = "http://localhost:8000/api/v1/project/12345/terraform/state/lock/terraform.lock"
+    unlock_address = "http://localhost:8000/api/v1/project/12345/terraform/state/unlock/terraform.lock"
+    unlock_method = "DELETE"
+    lock_method = "POST"
+  }
 }
