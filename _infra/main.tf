984053d0c6a566cebbd85a832691b817fcc2f84e
nasser.gonzalez@gmail.com <nasser.gonzalez@gmail.com>
2022-05-16 17:43:38 +0100

    No state

--- _infra/main.tf
+++ _infra/main.tf
@@ -6,11 +6,6 @@ module "app2" {
   source = "./modules/app2"
 }
 
-resource "aws_s3_bucket" "b" {
-  bucket = var.bucket_name
-  acl    = "public-read"
-}
-
 output "app-1-output-container-id" {
   value = module.app1.container_id
 }
