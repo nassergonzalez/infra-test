c84a867bba4194c107d015c954167d9dda20c87e
nasser.gonzalez@gmail.com <nasser.gonzalez@harness.io>
2022-11-01 15:01:59 +0000

    SQS

--- _infra/main.tf
+++ _infra/main.tf
@@ -1,3 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+  assume_role {
+    role_arn     = "arn:aws:iam::448640225317:role/nasserTest"
+    session_name = "SESSION_NAME"
+    external_id  = "EXTERNAL_ID"
+  }
+}
+
+
 resource "aws_sqs_queue" "terraform_queue" {
   name                      = "ngonzalez-queue"
   delay_seconds             = 90
