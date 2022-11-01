5da19d39bff9c4f97481662a7edac907f8e40a6b
nasser.gonzalez@gmail.com <nasser.gonzalez@harness.io>
2022-11-01 15:04:05 +0000

    SQS

--- _infra/main.tf
+++ _infra/main.tf
@@ -23,7 +23,4 @@ resource "aws_sqs_queue" "terraform_queue" {
   max_message_size          = 2048
   message_retention_seconds = 86400
   receive_wait_time_seconds = 10
-  tags = {
-    Environment = "nasserTest"
-  }
 }
